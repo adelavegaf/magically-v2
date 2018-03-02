f413fd4e16cd73c42cedd887ac68149229d1d861
adelavegaf <antoniodlavga@gmail.com>
2018-03-02 14:30:28 -0500

    Add search filters.

--- web/src/search/SearchFilter.js
+++ web/src/search/SearchFilter.js
@@ -8,7 +8,14 @@ import Typography from 'material-ui/Typography';
 const styles = theme => ({
   filterContainer: {
     marginTop: theme.spacing.unit * 3,
+    paddingBottom: theme.spacing.unit * 3,
     borderBottom: 'solid 1px #eeeeee'
+  },
+  filterColumn: {
+    marginTop: theme.spacing.unit * 3
+  },
+  filterColumnHeader: {
+    marginBottom: theme.spacing.unit * 3,
   },
   filterButton: {
     textAlign: 'right'
@@ -16,30 +23,70 @@ const styles = theme => ({
 });
 
 
-class SearchResults extends Component {
+class SearchFilter extends Component {
   constructor(props) {
     super(props);
     const {classes} = props;
     this.classes = classes;
   }
 
-  render() {
+  getFilterHeading(heading) {
     return (
-      <Grid container justify={'space-between'} alignItems={'center'} className={this.classes.filterContainer}>
-        <Grid item xs={6}>
-          <Typography>
-            About 10 results
+      <Grid item className={this.classes.filterColumnHeader}>
+        <Typography variant={'body2'}>
+          {heading}
+        </Typography>
+      </Grid>
+    );
+  }
+
+  getFilterColumnItems(items) {
+    return items.map((item, index) => {
+      return (
+        <Grid item key={index}>
+          <Typography variant={'caption'}>
+            {item}
           </Typography>
         </Grid>
-        <Grid item xs={6} className={this.classes.filterButton}>
-          <Button color={'primary'}>
-            <FilterIcon/>
-            Filter
-          </Button>
+      );
+    });
+  }
+
+  getFilterColumn(heading, items) {
+    return (
+      <Grid item xs={4} className={this.classes.filterColumn}>
+        <Grid container direction={'column'}>
+          {this.getFilterHeading(heading)}
+          {this.getFilterColumnItems(items)}
+        </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    return (
+      <Grid container className={this.classes.filterContainer}>
+        <Grid container justify={'space-between'} alignItems={'center'}>
+          <Grid item xs={6}>
+            <Typography>
+              About 10 results
+            </Typography>
+          </Grid>
+          <Grid item xs={6} className={this.classes.filterButton}>
+            <Button color={'primary'}>
+              <FilterIcon/>
+              Filter
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container spacing={24}>
+          {this.getFilterColumn('DATE', ['Today', 'This year', 'All time'])}
+          {this.getFilterColumn('OWNER', ['Anyone', 'Only others', 'Only me'])}
+          {this.getFilterColumn('SORT BY', ['Favorites', 'Upvotes', 'Completion Percentage'])}
         </Grid>
       </Grid>
     );
   }
 }
 
-export default withStyles(styles)(SearchResults);
+export default withStyles(styles)(SearchFilter);

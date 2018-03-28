dfb654f8dc0160356e268db71d91f331e7891384
Antonio de la Vega <antoniodlavga@gmail.com>
2018-03-27 21:31:45 -0500

    Add firebase to project

---
+++ web/src/firebase.js
@@ -0,0 +1,16 @@
+// Import the Firebase modules that you need in your app.
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/datastore';
+
+// Initalize and export Firebase.
+const config = {
+  apiKey: 'AIzaSyAskO3QYe979opV4QmRW3-tk6AwCrFRNmI',
+  authDomain: 'magically-188518.firebaseapp.com',
+  databaseURL: 'https://magically-188518.firebaseio.com',
+  projectId: 'magically-188518',
+  storageBucket: '',
+  messagingSenderId: '714997527027'
+};
+export default firebase.initializeApp(config);

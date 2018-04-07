c36902f1070191597720a9e5bdad7e9a7b6c8799
Antonio de la Vega <antoniodlavga@gmail.com>
2018-04-06 22:31:55 -0500

    Add image error parsing based on light house report.

--- audit_backend/main.js
+++ audit_backend/main.js
@@ -2,10 +2,29 @@ const chromeLauncher = require('chrome-launcher');
 const lighthouse = require('lighthouse');
 const lighthouseConfig = require('./lighthouse-config');
 const PubSub = require(`@google-cloud/pubsub`);
-const pubsub = new PubSub();
+const {URL} = require('url');
 
+const pubsub = new PubSub();
 const subscriptionName = 'audit-sub';
 const subscription = pubsub.subscription(subscriptionName);
+
+const getImageErrors = (lhr, url) => {
+  const errors = lhr.audits['image-alt'].details.items;
+  return errors
+    .filter(error => {
+      const regexMatch = error.snippet.match(/src="(.*?)"/);
+      return regexMatch && regexMatch.length > 1;
+    })
+    .map(error => {
+      const extractedURL = error.snippet.match(/src="(.*?)"/)[1];
+      const imgURL = new URL(extractedURL, url);
+      const domSelector = error.selector;
+      return {
+        imgURL: imgURL,
+        domSelector: domSelector
+      };
+    })
+};
 
 (async () => {
   const opts = {
@@ -21,10 +40,15 @@ const subscription = pubsub.subscription(subscriptionName);
     message.ack();
 
     const payload = JSON.parse(message.data);
-    console.log('URL', payload.websiteUrl);
+    const {websiteUrl} = payload;
+    console.log(`Received Message ${message.id} with URL: ${websiteUrl}`);
 
-    const lhr = await lighthouse(payload.websiteUrl, opts, lighthouseConfig);
+    const lhr = await lighthouse(websiteUrl, opts, lighthouseConfig);
     console.log(`Lighthouse score: ${lhr.score}`);
+
+    const imageErrors = getImageErrors(lhr, websiteUrl);
+    console.log('image errors');
+    console.log(JSON.stringify(imageErrors));
   }));
 
 })();

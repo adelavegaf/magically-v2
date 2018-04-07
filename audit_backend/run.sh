1f4e8011268fc21e80e62b52c58642101be54ff6
Antonio de la Vega <antoniodlavga@gmail.com>
2018-04-07 16:04:25 +0000

    Modified script to pull proper version of google chrome.

--- audit_backend/run.sh
+++ audit_backend/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CHROME_PATH="/opt/stickmanventures/chrome-headless/headless_shell"
+export CHROME_PATH="/usr/bin/google-chrome-stable"
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/credentials.json"
 
 node index
--- audit_backend/setup.sh
+++ audit_backend/setup.sh
@@ -1,39 +1,18 @@
 #!/usr/bin/env bash
 
-curl https://storage.googleapis.com/chrome-builds/headless/chrome-headless.deb > chrome-headless.deb
-
 apt-get update && apt-get install -y \
-  build-essential \
-  software-properties-common \
-  ca-certificates \
-  byobu curl git htop man unzip vim wget \
-  sudo \
-  gconf-service \
-  libcurl3 \
-  libexif-dev \
-  libgconf-2-4 \
-  libglib2.0-0 \
-  libgl1-mesa-dri \
-  libgl1-mesa-glx \
-  libnspr4 \
-  libnss3 \
-  libpango1.0-0 \
-  libv4l-0 \
-  libxss1 \
-  libxtst6 \
-  libxrender1 \
-  libx11-6 \
-  libxft2 \
-  libfreetype6 \
-  libc6 \
-  zlib1g \
-  libpng12-0 \
-  wget \
-  apt-utils \
-  xdg-utils \
-  --no-install-recommends && \
-  dpkg -i './chrome-headless.deb' && \
-  curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash - && \
-  sudo apt-get install -y nodejs && \
-  sudo apt-get install -y libnss3 && \
-  rm -rf /var/lib/apt/lists/*
+    apt-transport-https \
+	ca-certificates \
+	curl \
+  	gnupg \
+	--no-install-recommends \
+	&& curl -sSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
+	&& echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list \
+	&& apt-get update && apt-get install -y \
+	google-chrome-stable \
+	--no-install-recommends \
+	&& apt-get purge --auto-remove -y curl gnupg \
+	&& rm -rf /var/lib/apt/lists/*
+    curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash - && \
+    sudo apt-get install -y nodejs && \
+    rm -rf /var/lib/apt/lists/*

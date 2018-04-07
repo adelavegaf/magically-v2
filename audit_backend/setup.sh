58cd2f1c9aae7900ac2bc782215c7c1c53c0c2a6
Antonio de la Vega <antoniodlavga@gmail.com>
2018-04-07 15:53:06 +0000

    Remove unnecessary line.

--- audit_backend/setup.sh
+++ audit_backend/setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 curl https://storage.googleapis.com/chrome-builds/headless/chrome-headless.deb > chrome-headless.deb
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 apt-get update && apt-get install -y \
   build-essential \

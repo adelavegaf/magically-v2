d04d1870266c6402d183cf93ad7132cfbab0c289
Antonio de la Vega <antoniodlavga@gmail.com>
2018-04-15 16:33:52 -0500

    Apply fix to webpage.

--- chrome_extension/public/app/background.js
+++ chrome_extension/public/app/background.js
@@ -10,6 +10,61 @@ const initFirebase = () => {
   firebase.initializeApp(config);
 };
 
+const wrapFixBodyInCommonComponent = (domSelector, body) => {
+  return `{
+    const domElement = document.querySelector('${domSelector}');
+    if (domElement) {
+      ${body}
+    }
+  }`;
+};
+
+const applyContrastErrorsFix = (contrastErrors) => {
+  return Object
+    .entries(contrastErrors)
+    .filter(([, error]) => error.isFixed)
+    .map(([, error]) => {
+      const body = `
+      domElement.style.background = '${error.backgroundColor} !important';
+      domElement.style.color = '${error.foregroundColor} !important';
+      `;
+      return wrapFixBodyInCommonComponent(error.domSelector, body);
+    });
+};
+
+const applyImageErrorsFix = (imageErrors) => {
+  return Object
+    .entries(imageErrors)
+    .filter(([, error]) => error.isFixed)
+    .map(([, error]) => {
+      const body = `
+      domElement.alt = '${error.description}';
+      `;
+      return wrapFixBodyInCommonComponent(error.domSelector, body);
+    });
+};
+
+const applyLangErrorsFix = (langErrors) => {
+  return Object
+    .entries(langErrors)
+    .filter(([, error]) => error.isFixed)
+    .map(([, error]) => {
+      const body = `
+      domElement.lang = '${error.lang}';
+      `;
+      return wrapFixBodyInCommonComponent(error.domSelector, body);
+    });
+};
+
+const getFixFromProject = (project) => {
+  const {contrastErrors, imageErrors, langErrors} = project.errors;
+  const contrastFix = applyContrastErrorsFix(contrastErrors);
+  const imageFix = applyImageErrorsFix(imageErrors);
+  const langFix = applyLangErrorsFix(langErrors);
+  const generalFix = [...contrastFix, ...imageFix, ...langFix];
+  return generalFix.join('');
+};
+
 chrome.tabs.onUpdated.addListener((tabId, {status}, tab) => {
   if (status === 'complete') {
 
@@ -21,9 +76,19 @@ chrome.tabs.onUpdated.addListener((tabId, {status}, tab) => {
       .firestore()
       .collection('projects')
       .where('websiteUrl', '==', tab.url)
+      .where('isLoading', '==', false)
       .get()
       .then(projects => {
-        console.log(projects);
+        if (!projects.docs.length) {
+          return;
+        }
+        // TODO(adelavega): Analyze which project should be loaded.
+        const selectedProject = projects.docs[0].data();
+        const fix = getFixFromProject(selectedProject);
+        console.log(fix);
+        chrome.tabs.executeScript({
+          code: fix
+        });
       });
   }
 });

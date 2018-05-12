39d4bd59b76e538623bf9a857463a6401675b25a
Antonio de la Vega <antoniodlavga@gmail.com>
2018-05-11 22:23:43 -0500

    Refactor firebase initialization and fix logic out of index.

---
+++ chrome_extension/background/src/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: 'AIzaSyAskO3QYe979opV4QmRW3-tk6AwCrFRNmI',
+  authDomain: 'magically-188518.firebaseapp.com',
+  databaseURL: 'https://magically-188518.firebaseio.com',
+  projectId: 'magically-188518',
+  storageBucket: '',
+  messagingSenderId: '714997527027'
+};
+export default firebase.initializeApp(config);
--- chrome_extension/background/src/index.js
+++ chrome_extension/background/src/index.js
@@ -2,6 +2,7 @@ import {createStore} from 'redux';
 import rootReducer from './reducers';
 import {wrapStore} from 'react-chrome-redux';
 import Fix from './Fix';
+import firebase from './firebase';
 
 // TODO(adelavega): consider race condition between storage get, and firebase set.
 
@@ -11,25 +12,8 @@ wrapStore(store, {
   portName: 'magically'
 });
 
-const initFirebase = () => {
-  const config = {
-    apiKey: 'AIzaSyAskO3QYe979opV4QmRW3-tk6AwCrFRNmI',
-    authDomain: 'magically-188518.firebaseapp.com',
-    databaseURL: 'https://magically-188518.firebaseio.com',
-    projectId: 'magically-188518',
-    storageBucket: '',
-    messagingSenderId: '714997527027'
-  };
-  firebase.initializeApp(config);
-};
-
 chrome.tabs.onUpdated.addListener((tabId, {status}, tab) => {
   if (status === 'complete') {
-
-    if (!firebase.apps.length) {
-      initFirebase();
-    }
-
     firebase
       .firestore()
       .collection('projects')

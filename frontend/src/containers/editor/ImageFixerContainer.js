c94c829da161d8eb3bbe09481de9dd89861029ea
Antonio de la Vega <antoniodlavga@gmail.com>
2018-05-15 13:11:02 -0500

    No description checkbox bug fix

--- frontend/src/containers/editor/ImageFixerContainer.js
+++ frontend/src/containers/editor/ImageFixerContainer.js
@@ -28,7 +28,7 @@ class ImageFixerContainer extends Component {
     const {imageErrors, imageErrorsCount} = this.props;
     const index = this.state.currentErrorKey;
     const error = imageErrors[index];
-    const hasNoDescription = error && error.description ? error.isFixed && error.description.length === 0 : false;
+    const hasNoDescription = error && error.description !== undefined ? error.isFixed && error.description.length === 0 : false;
     return React.createElement(ImageFixer, {
         isOwner: this.props.isOwner,
         sideListMaxHeight: this.props.sideListMaxHeight,
